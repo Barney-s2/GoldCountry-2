a40ac346dc9bd44d47d5667e9985b765aa2b6e5a
Barney-s2 <Barney_s2@yahoo.com>
2020-11-26 16:19:57 -0800

    placeholder

---
+++ app/src/main/java/com/timetorevenue/openapi/repository/main/cache/abstraction/BlogPostDataSource.kt
@@ -0,0 +1,5 @@
+package com.timetorevenue.openapi.repository.main.cache.abstraction
+//package com.codingwithmitch.cleannotes.business.data.cache.abstraction
+
+interface BlogPostDataSource {
+}
---
+++ app/src/main/java/com/timetorevenue/openapi/repository/main/cache/implementation/BlogPostCacheSourceImpl.kt
@@ -0,0 +1,5 @@
+package com.timetorevenue.openapi.repository.main.cache.implementation
+//package com.codingwithmitch.cleannotes.business.data.cache.implementation
+
+class BlogPostCacheSourceImpl {
+}
